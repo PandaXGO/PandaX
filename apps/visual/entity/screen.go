5852896a325997e38bcdb82dee1376ef9cdc0243
XM-GO <93296511+XM-GO@users.noreply.github.com>
2023-04-13 17:14:27 +0800

    大屏

--- apps/visual/entity/screen.go
+++ apps/visual/entity/screen.go
@@ -27,9 +27,9 @@ type VisualScreen struct {
 	ScreenId       string `gorm:"primary_key;" json:"screenId"`
 	GroupId        int64  `gorm:"screenGroup;type:int;comment:分组Id" json:"groupId"`
 	ScreenName     string `gorm:"screenName;type:varchar(50);comment:名称" json:"screenName"`
-	ScreenDataJson string `gorm:"screenDataJson;type:varchar(50);comment:Json数据" json:"screenDataJson"`
-	ScreenBase64   string `gorm:"screenBase64;type:varchar(50);comment:Base64缩略图" json:"screenBase64"` //缩略图 base64
-	ScreenRemark   string `gorm:"screenRemark;type:varchar(50);comment:说明" json:"screenRemark"`
+	ScreenDataJson string `gorm:"screenDataJson;type:text;comment:Json数据" json:"screenDataJson"`
+	ScreenBase64   string `gorm:"screenBase64;type:text;comment:Base64缩略图" json:"screenBase64"` //缩略图 base64
+	ScreenRemark   string `gorm:"screenRemark;type:varchar(255);comment:说明" json:"screenRemark"`
 	Status         string `gorm:"status;type:varchar(1);comment:状态" json:"status"`
 	Creator        string `json:"creator"` //创建者
 	model.BaseModel
