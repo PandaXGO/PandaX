748751c2abb4f027d4018fe166186e0964dc6da5
勤快的小晴同学 <941403820@qq.com>
2024-01-26 02:13:07 +0000

    添加数据库创建语句
    Signed-off-by: 勤快的小晴同学 <941403820@qq.com>

--- resource/pandax_iot.sql
+++ resource/pandax_iot.sql
@@ -17,6 +17,8 @@
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
+CREATE DATABASE IF NOT EXISTS pandax_iot;
+
 -- ----------------------------
 -- Table structure for casbin_rule
 -- ----------------------------
